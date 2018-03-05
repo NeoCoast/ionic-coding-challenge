a85c5af276b484d9c575768e19cc0471b89f5416
Nicolas Tinte <nicolas@neocoast.com>
2018-03-05 15:39:39 -0300

    SplashScreen

---
+++ src/pages/splash/splash.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../../pages/home/home';
+
+@IonicPage()
+@Component({
+  selector: 'page-splash',
+  templateUrl: 'splash.html',
+})
+export class SplashPage {
+
+  constructor(public splashScreen: SplashScreen, public navCtrl: NavController) { }
+
+  ionViewDidEnter() {
+    this.splashScreen.hide();
+
+    setTimeout(() => {
+      this.navCtrl.setRoot(HomePage);
+    }, 2000);
+  
+  }
+}
